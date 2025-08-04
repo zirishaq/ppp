327145f90597de39d3f8932bc70e8ad53a395660
zirishaq <diharhaq0606@gmail.com>
2025-08-04 11:00:57 +0700

    apa

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            Forms\Components\TextInput::make('nama')
+            ->label('Nama Lengkap')
+            ->required()
+            ->maxLength(225),
+
+            Forms\Components\TextInput::make('kelas')
+            ->label('kelas')
+            ->required()
+            ->maxLength(50),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+            Tables\Columns\TextColumn::make('nama')->label('nama'),
+            Tables\Columns\TextColumn::make('kelas')->label('kelas'),
+            Tables\Columns\TextColumn::make('created_at')->dateTime('d M Y H:i'),
+            ])
+
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
